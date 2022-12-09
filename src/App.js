44aac6111afa39e73f51417e6121d00eb5814a65
Moh Ariful <nextkoding@gmail.com>
2022-12-09 07:53:00 +0700

    adding remove function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./EmpListing";
+import EmpCreate from "./EmpCreate";
+import EmpDetail from "./EmpDetail";
+import EmpEdit from "./EmpEdit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React JS CRUD Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path="employee/create" element={<EmpCreate />}></Route>
+
+          <Route path="employee/detail/:empid" element={<EmpDetail />}></Route>
+          <Route path="employee/edit/:empid" element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
